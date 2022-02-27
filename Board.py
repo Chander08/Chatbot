38edc3c3fc4a977bacd18f5aa39a3be03b0281e2
Chander08 <suhailchander101@gmail.com>
2022-02-27 03:43:35 -0500

    updated board

--- Board.py
+++ Board.py
@@ -2,9 +2,11 @@ import pygame
 import numpy as np 
 
 pygame.init() 
-window = pygame.display.set_mode((1200,700))
+window = pygame.display.set_mode((700,700))
 pygame.display.set_caption("Sudoku") #changes window name
-
+white = (255,255,255) #colors defined in RGB format
+black = (0,0,0)
+red = (255,0,0)
 
 class Board:
     def __init__(self, initial_values):
@@ -13,17 +15,44 @@ class Board:
             initial_values - Not sure what Smita will use so will adjust accordingly - using just a list for now
             (list values from left to right top to bottom, leaving blanks as a space for now?)
         """
-
+        
         self.initial_values = initial_values
 
     def create_board():
         #create a board using initial values in pygame
+        square_size = 65
+        list_of_positions = [] #list storing the positions of all of the rectangles to input numbers into later
+        for i in range(1, 10): #since 9 boxes needed
+            for j in range(1,10):
+                pygame.draw.rect(window, white, (i*square_size, j*square_size, square_size, square_size), 2)
+                #rectangle_position = [i*square_size, j*square_size]
+                #list_of_positions.append(rectangle_position)
+
+        #for i in range(1,4): #making larger squares to add the border effect
+         #   for j in range(1,4): 
+          #      pygame.draw.rect(window, red, (i*square_size*3, j*square_size*3, square_size*3, square_size*3), 2)
+        #pygame.display.update()
 
     def update_board(): 
         #change board values for empty squares depending on user input 
+        pass
 
     def solve_board():
         #run the final code solver function and take the output to populate this board
+        pass
 
     def clear_board():
         #either this or delete board and create a new one? 
+        pass
+board1 = Board
+board1.create_board()
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+
+pygame.quit()
