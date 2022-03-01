27d921bb61776f191f833a60978d0f168ed2d810
Chander08 <suhailchander101@gmail.com>
2022-02-28 19:19:10 -0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -4,6 +4,7 @@ import numpy as np
 pygame.init() 
 window = pygame.display.set_mode((700,700))
 pygame.display.set_caption("Sudoku") #changes window name
+font = pygame.font.SysFont('Arial', 50) #font and size
 white = (255,255,255) #colors defined in RGB format
 black = (0,0,0)
 red = (255,0,0)
@@ -13,13 +14,12 @@ class Board:
     def __init__(self, initial_values):
         """Initializes the board with the given initial values.
         INPUTS:
-            initial_values - Not sure what Smita will use so will adjust accordingly - using just a list for now
-            (list values from left to right top to bottom, leaving blanks as a space for now?)
+            initial_values - Not sure what Smita will use so will adjust accordingly - using just 9x9 array for now
         """
         
-        self.initial_values = initial_values
+        self.initial_values = (initial_values)
 
-    def create_board():
+    def create_board(self):
         '''
         creates the 9x9 sudoku board to play on based on the initial values of the initialized board
         '''
@@ -29,9 +29,12 @@ class Board:
         list_of_positions = [] #list storing the positions of all of the rectangles to input numbers into later
         for i in range(1, 10): #since 9 boxes needed
             for j in range(1,10):
-                pygame.draw.rect(window, grey, (i*square_size, j*square_size, square_size, square_size), 2)
-                rectangle_position = [i*square_size, j*square_size]
-                list_of_positions.append(rectangle_position)
+                x_pos = j*square_size
+                y_pos = i*square_size
+                pygame.draw.rect(window, grey, (x_pos, y_pos, square_size, square_size), 2)
+                #rectangle positions stored as left, right, top, bottom of a square:
+                rectangle_positions = [x_pos, x_pos + square_size, y_pos, y_pos + square_size] 
+                list_of_positions.append(rectangle_positions)
 
         for i in range(0,3): #making larger squares to add the border effect
             i += 1/3 #gross way to make it match up
@@ -39,36 +42,61 @@ class Board:
                 j += 1/3
                 pygame.draw.rect(window, white, (i*square_size*3, j*square_size*3, square_size*3, square_size*3), 2)
         pygame.display.update()
-        return(list_of_positions)
-
-        '''
-        notes for next time ig
-        need some organized way to identify the square being selected and to put a textbox there. Can use the list of 
-        positions and centre the number text box on that and find the dimensions of the box surrounding that. Then use pygame 
-        cursor detector to see if its in that range and if it is clicked it becomes some sort of text box?
-        
-        '''
+        pos_count = 0
+        #the next part can probably be added to the first for loop but idk if its worth it
+        for i in self.initial_values:
+            for j in i:
+                if j != 0: #if 0 dont input any number
+                    display_text = font.render(str(j), True, white)
+                    window.blit(display_text, (list_of_positions[pos_count][0]+20, list_of_positions[pos_count][2]+5))
+                pos_count += 1
+        pygame.display.update()
 
     def update_board(): 
+        '''
+        notes for next time ig
+        Can use the list_of_positions from create_board to identify the regions of each square
+        then add some sort of text add thing using pygame to let users input numbers
+        cursor detector to see if its in that range and if it is clicked it becomes some sort of text box?
+        also need some way of identifying if the square is already filled or not, since we dont want them to overwrite
+        the initial board values
+        '''
         #change board values for empty squares depending on user input 
         pass
 
     def solve_board():
+        '''
+        can just take the solved board array and create board using it
+        '''
         #run the final code solver function and take the output to populate this board
         pass
 
-    def clear_board():
-        #either this or delete board and create a new one? 
+    def check_board():
+        #compares solution with user input values
         pass
-board1 = Board
-board1.create_board()
 
+    def new_game():
+        #create new board
+        pass
+
+solvable_example_1 = np.array([[0,0,7,4,9,1,6,0,5], 
+                                [2,0,0,0,6,0,3,0,9],
+                                [0,0,0,0,0,7,0,1,0],
+                                [0,5,8,6,0,0,0,0,4],
+                                [0,0,3,0,0,0,0,9,0],
+                                [0,0,6,2,0,0,1,8,7],
+                                [9,0,4,0,7,0,0,0,2],
+                                [6,7,0,8,3,0,0,0,0],
+                                [8,1,0,0,4,5,0,0,0]])
+
+board1 = Board(solvable_example_1)
+board1.create_board()
 
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    #have to add user input stuff here
     pygame.display.update()
-
 pygame.quit()
